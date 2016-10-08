ddb93ad82678b5af01f62c7399c757e1d650a855
CrisFeo <feo.cris@gmail.com>
2016-10-08 11:47:08 -0700

    Add option to skip install steps

--- install.sh
+++ install.sh
@@ -1,61 +1,62 @@
 #! /bin/bash
 set -e
 
-
-# Install helpers
-brewAdd() {
-  brew install "$1" || brew upgrade "$1"
-}
-
-
-# Homebrew dependencies
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || :
-
-brew update
-
-brewAdd ag
-brewAdd fzf
-brewAdd git
-brewAdd go
-brewAdd gotags
-brewAdd mopidy
-brewAdd mpc
-brewAdd ncmpc
-brewAdd neovim
-brewAdd nvm
-brewAdd reattach-to-user-namespace
-brewAdd tmux
-brewAdd watch
-
-brew tap caskroom/versions
-
-brew cask install "karabiner"
-brew cask install "iterm2-nightly"
-brew cask install "mattr-slate"
-brew cask install "seil"
+if [ "$1" != '--skip-install' ]; then
+  # Install helpers
+  brewAdd() {
+    brew install "$1" || brew upgrade "$1"
+  }
 
 
-# Node dependencies
-# shellcheck source=/dev/null
-source "$HOME/.nvm/nvm.sh"
-nvm install node
-npm install -g http-server
-npm install -g underscore-cli
+  # Homebrew dependencies
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || :
+
+  brew update
+
+  brewAdd ag
+  brewAdd fzf
+  brewAdd git
+  brewAdd go
+  brewAdd gotags
+  brewAdd mopidy
+  brewAdd mpc
+  brewAdd ncmpc
+  brewAdd neovim
+  brewAdd nvm
+  brewAdd reattach-to-user-namespace
+  brewAdd tmux
+  brewAdd watch
+
+  brew tap caskroom/versions
+
+  brew cask install "karabiner"
+  brew cask install "iterm2-nightly"
+  brew cask install "mattr-slate"
+  brew cask install "seil"
 
 
-# Neovim dependencies
-unset PYTHONPATH # Just in case to make sure install doesn't fail
-brewAdd python3
-pip install neovim
-pip3 install neovim
-sudo gem install neovim
+  # Node dependencies
+  # shellcheck source=/dev/null
+  source "$HOME/.nvm/nvm.sh"
+  nvm install node
+  npm install -g http-server
+  npm install -g underscore-cli
+
+
+  # Neovim dependencies
+  unset PYTHONPATH # Just in case to make sure install doesn't fail
+  brewAdd python3
+  pip install neovim
+  pip3 install neovim
+  sudo gem install neovim
+fi
 
 
 # Install user Home dotfiles
 FILES_SOURCE=$(find "$PWD/home" -depth 1)
 FILES_DEST=${FILES_SOURCE//$PWD\/home/$HOME}
 cd "$HOME" || exit
-xargs -n 1 rm -r <<< "$FILES_DEST"
+xargs -n 1 rm -rf <<< "$FILES_DEST"
 xargs -n 1 ln -s <<< "$FILES_SOURCE"
 cd - || exit
 
